947254768fc5e4e490646c77760fb136a6dd7a45
TraBee <tra8brown@gmail.com>
2021-05-20 12:20:22 -0600

    ability to edit task dates

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,5 @@
 var tasks = {};
-//
-//hi :)
+
 var createTask = function(taskText, taskDate, taskList) {
     // create elements that make up a task item
     var taskLi = $("<li>").addClass("list-group-item");
@@ -63,39 +62,80 @@ $("#task-form-modal").on("shown.bs.modal", function() {
 
 // save button in modal was clicked
 $("#task-form-modal .btn-primary").click(function() {
-    // get form values
-    var taskText = $("#modalTaskDescription").val();
-    var taskDate = $("#modalDueDate").val();
+        // get form values
+        var taskText = $("#modalTaskDescription").val();
+        var taskDate = $("#modalDueDate").val();
 
-    if (taskText && taskDate) {
-        createTask(taskText, taskDate, "toDo");
+        if (taskText && taskDate) {
+            createTask(taskText, taskDate, "toDo");
 
-        // close modal
-        $("#task-form-modal").modal("hide");
+            // close modal
+            $("#task-form-modal").modal("hide");
 
-        // save in tasks array
-        tasks.toDo.push({
-            text: taskText,
-            date: taskDate
+
+            // save in tasks array
+            tasks.toDo.push({
+                text: taskText,
+                date: taskDate
+
+            });
+            saveTasks();
+        }
+
+        // blur callback () 5.1.6
+        $("list-group").on("click", "p", function() {
+            var text = $(this)
+                .text()
+                .trim();
+            console.log(this);
         });
 
-        saveTasks();
-    }
+        $(".list-group").on("blur", "textarea", function() { //blur event will trigger 5.1.6
 
-    //on callback () 5.1.6
-    $("list-group").on("click", "p", function() {
-        console.log(this);
-    });
+            // get the textarea's current value/text 5.1.6
+            var text = $(this)
+                .val()
+                .trim();
 
-    // remove all tasks
-    $("#remove-tasks").on("click", function() {
-        for (var key in tasks) {
-            tasks[key].length = 0;
-            $("#list-" + key).empty();
-        }
-        saveTasks();
-    });
+            // get the parent ul's id attribute 5.1.6
+            var status = $(this)
+                .closest(".list-group")
+                .attr("id")
+                .replace("list-", "");
 
+            // get the task's position in the list of other li elements 5.1.6
+            var index = $(this)
+                .closest(".list-group-item")
+                .index();
+
+            tasks[status][index].text = text; //5.1.6
+            saveTasks();
+
+            //recreate p element 5.1.6
+            var taskP = $("<p>")
+                .addClass("m-1")
+                .text(text);
+
+            //replace textarea with p element
+            $(this).replaceWith(taskP);
+        });
+
+        var textInput = $("<textarea>") //5.1.6
+            .addClass("form-control")
+            .val(text);
+        $(this).replaceWith(textInput);
+        textInput.trigger("focus"); //highlights the input box 
+
+        // remove all tasks
+        $("#remove-tasks").on("click", function() {
+            for (var key in tasks) {
+                tasks[key].length = 0;
+                $("#list-" + key).empty();
+            }
+
+            saveTasks();
+        });
+
+    })
     // load tasks for the first time
-    loadTasks();
-})
+loadTasks();
