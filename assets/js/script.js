4cae27a6b17d0f574fbe6f4ef2793b9b5b5ed1c5
TraBee <tra8brown@gmail.com>
2021-05-27 17:55:45 -0600

    updated with code

--- assets/js/script.js
+++ assets/js/script.js
@@ -175,6 +175,28 @@ $("#remove-tasks").on("click", function() {
     }
     saveTasks();
 });
+//   sortable ()
+$(".card .list-group").sortable({
+    connectWith: $(".card .list-group"),
+    scroll: false,
+    tolerance: "pointer",
+    helper: "clone",
+    activate: function(event) {
+        console.log("activate", this);
+    },
+    deactivate: function(event) {
+        console.log("deactivate", this);
+    },
+    over: function(event) {
+        console.log("over", event.target);
+    },
+    out: function(event) {
+        console.log("out", event.target);
+    },
+    update: function(event) {
+        console.log($(this).children());
+    }
+});
 
 // load tasks for the first time
 loadTasks();
